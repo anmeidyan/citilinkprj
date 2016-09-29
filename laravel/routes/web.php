124ffeb31a145d1b420ac798931a11fa241d200e
anmeidyan <ameidyan@gmail.com>
2016-09-29 15:49:26 +0700

    dariandreas

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 //------------------------------------------------------------------ FRONT START
-Route::get('/', function () {
+Route::get('/dariandreas', function () {
     return view('front.home');
 });
 Route::get('/layanan', function () {
